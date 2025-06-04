fc46952903f905d730ace1c64ed7ab741f7f0a55
BrunoE <brunoenrigo@gmail.com>
2025-06-04 00:13:02 -0300

    completed change image feature from profile page

--- src/app/API/userId/route.ts
+++ src/app/API/userId/route.ts
@@ -1,16 +1,15 @@
 'use server'
 
-import { DecodeToken } from "@/actions";
 import { cookies } from "next/headers";
 
-const api_URL = 'http:localhost:3000'; // change URL to env variable
+const api_URL = 'http:localhost:3000/api/users/'; // change URL to env variable
 
 export async function userById(userId: number) {
     const token = (await cookies()).get('auth_token')?.value;
 
     if (token) {
         try {
-            const userInfo = await fetch(`${api_URL}/api/users/${userId}`, {
+            const userInfo = await fetch(`${api_URL}${userId}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -30,6 +29,28 @@ export async function userById(userId: number) {
     }
 }
 
-// export async function updateProfilePicture() {
+export async function updateProfilePicture(file: File): Promise<any> {
+    const token = (await cookies()).get('auth_token')?.value;
+    const formData = new FormData();
+    formData.append("file", file);
 
-// }
+    console.log('formData: ', formData);
+
+    if (token) {
+        try {
+            const success = await fetch(`${api_URL}profile-image`, {
+                method: 'PATCH',
+                body: formData,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            console.log(success);
+            return success.json();
+        } catch (err) {
+            console.error('Upload failed: ', err)
+            return err;
+        }
+    }
+}

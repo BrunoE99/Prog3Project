8c1d033d451d48a752ce95562bddd7f178807106
Andres Rabbione <andresrabbione@gmail.com>
2025-06-11 23:36:57 -0300

    Minor adjustments to the create group page

--- src/app/group/create/page.tsx
+++ src/app/group/create/page.tsx
@@ -2,6 +2,7 @@
 
 import { useActionState, useState } from "react";
 import { createGroup } from "../actions";
+import { redirect } from "next/navigation";
 
 interface MovieComponents {
   id: number;
@@ -78,57 +79,66 @@ interface ReviewComponents {
 
 export default function GroupCreate() {
   const [descriptionGiven, setGiven] = useState(false);
-  const [name, setName] = useState<string | undefined>(undefined);
   const [state, action, pending] = useActionState(createGroup, undefined);
 
   return (
-    <form action={action} className="min-h-screen bg-[#001d3d]">
-      <div className="flex flex-col gap-5 p-5">
-        <input
-          className="hidden"
-          name="descriptionGiven"
-          id="descriptionGiven"
-          readOnly
-          value={descriptionGiven?.toString()}
-        />
-        <div className="flex flex-col items-start justify-center gap-2">
-          <span className="font-semibold text-2xl">Name</span>
-          <input
-            className="border border-[#545454b7] w-full mb-1 p-1"
-            type="text"
-            id="group-name"
-            name="group-name"
-            required
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-col items-start justify-center gap-2">
-          <span className="font-semibold text-2xl">Description</span>
-          <textarea
-            className="w-full border border-[#545454b7] mb-1 p-1"
-            name="group-description"
-            id="group-description"
-            onChange={(e) => {
-              if (e.target.value === "") {
-                setGiven(false);
-              } else {
-                setGiven(true);
-              }
-            }}
-          ></textarea>
-        </div>
-        <div className="flex justify-end items-center">
-          <input
-            className={`bg-blue-700 rounded-md text-xl m-5 p-1 ${
-              pending ? "" : "cursor-pointer hover:bg-blue-600"
-            }`}
-            type="submit"
-            id="submit"
-            name="submit"
-            disabled={pending}
-          />
-        </div>
+    <div className="min-h-screen bg-[#001d3d]">
+      <div className="flex flex-row gap-3 p-5">
+        <button
+          className="text-4xl font-semibold opacity-60 cursor-pointer hover:opacity-100"
+          onClick={() => redirect("/group")}
+        >
+          &lt;
+        </button>
+        <span className="text-4xl font-semibold">Create Group</span>
       </div>
-    </form>
+      <form action={action}>
+        <div className="flex flex-col gap-6 p-5">
+          <input
+            className="hidden"
+            name="descriptionGiven"
+            id="descriptionGiven"
+            readOnly
+            value={descriptionGiven?.toString()}
+          />
+          <div className="flex flex-col items-start justify-center gap-2">
+            <span className="font-semibold text-2xl">Name</span>
+            <input
+              className="border border-[#545454b7] w-full mb-1 p-1"
+              type="text"
+              id="group-name"
+              name="group-name"
+              required
+            />
+          </div>
+          <div className="flex flex-col items-start justify-center gap-2">
+            <span className="font-semibold text-2xl">Description</span>
+            <textarea
+              className="w-full border border-[#545454b7] mb-1 p-1"
+              name="group-description"
+              id="group-description"
+              onChange={(e) => {
+                if (e.target.value === "") {
+                  setGiven(false);
+                } else {
+                  setGiven(true);
+                }
+              }}
+            ></textarea>
+          </div>
+          <div className="flex justify-end items-center">
+            <input
+              className={`bg-blue-700 rounded-md text-xl m-5 p-1 ${
+                pending ? "" : "cursor-pointer hover:bg-blue-600"
+              }`}
+              type="submit"
+              id="submit"
+              name="submit"
+              disabled={pending}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
